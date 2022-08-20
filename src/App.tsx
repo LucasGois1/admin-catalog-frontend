0e9e03411cc8b65af0bfe476172df2c3e1eff5d6
LucasGois1 <lucassampaiodegois1@gmail.com>
2022-08-20 10:48:37 -0300

    feat: :sparkles: add new components
    Header and Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import "./App.css";
-import { Button } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { Box, ThemeProvider } from "@mui/system";
+import { Header } from "./components/Header";
+import { Layout } from "./components/Layout";
+
+const theme = createTheme({});
 
 function App() {
-  return <Button variant="contained">Ola!</Button>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Box
+        component="main"
+        sx={{
+          height: "100vh",
+          // backgroundColor: "#000",
+        }}
+      >
+        <Header />
+        <Layout>
+          <h1>Ola mundo!</h1>
+        </Layout>
+      </Box>
+    </ThemeProvider>
+  );
 }
 
 export default App;
