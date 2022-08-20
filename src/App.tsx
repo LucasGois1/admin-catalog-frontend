6b6f78680dc01ce1d89e55cf07641f7389e6abcb
LucasGois1 <lucassampaiodegois1@gmail.com>
2022-08-20 13:01:50 -0300

    feat: :sparkles: add Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
 import React from "react";
 import "./App.css";
-import { createTheme } from "@mui/material";
+
 import { Box, ThemeProvider } from "@mui/system";
 import { Header } from "./components/Header";
 import { Layout } from "./components/Layout";
-
-const theme = createTheme({});
+import { appTheme } from "./config/theme";
+import { Typography } from "@mui/material";
+import { Link, Route, Routes } from "react-router-dom";
+import ListCategories from "./features/categories/ListCategories";
+import CreateCategory from "./features/categories/CreateCategory";
+import EditCategory from "./features/categories/EditCategory";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={appTheme}>
       <Box
         component="main"
         sx={{
           height: "100vh",
-          // backgroundColor: "#000",
+          backgroundColor: (theme) => theme.palette.grey[900],
         }}
       >
         <Header />
         <Layout>
-          <h1>Ola mundo!</h1>
+          <Routes>
+            <Route path="/" element={<ListCategories />} />
+            <Route path="/categories" element={<ListCategories />} />
+            <Route path="/categories/create" element={<CreateCategory />} />
+            <Route path="/categories/edit/:id" element={<EditCategory />} />
+
+            <Route
+              path="*"
+              element={
+                <Box>
+                  <Typography variant="h1">404</Typography>
+                  <Typography variant="h2">Page not found</Typography>
+                </Box>
+              }
+            />
+          </Routes>
         </Layout>
       </Box>
     </ThemeProvider>
