764ad079f0e291ea61632377b643d517107d8c89
LucasGois1 <lucassampaiodegois1@gmail.com>
2022-08-20 14:00:57 -0300

    feat: :sparkles: add Redux
    First version of our category slice

--- src/features/categories/ListCategories.tsx
+++ src/features/categories/ListCategories.tsx
@@ -1,10 +1,49 @@
-import { Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import React from "react";
+import { useAppSelector } from "../../app/hooks";
+import { selectCategories, Category } from "./categorySlice";
+import { Link } from "react-router-dom";
+import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
 
 export default function ListCategories() {
+  const categories = useAppSelector(selectCategories);
+
+  const rows: GridRowsProp = categories.map(
+    ({ id, name, description, created_at }) => ({
+      id,
+      name,
+      description,
+      created_at,
+    })
+  );
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 200 },
+    { field: "name", headerName: "Name", width: 150 },
+    { field: "description", headerName: "Description", width: 300 },
+  ];
+
   return (
-    <Typography variant="h3" component="h1">
-      Category List Page
-    </Typography>
+    <Box maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Box display="flex" justifyContent="flex-end">
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to="/categories/create"
+          style={{ marginBottom: "1rem" }}
+        >
+          New Category
+        </Button>
+      </Box>
+
+      <div style={{ height: 500, width: "100%" }}>
+        <DataGrid rows={rows} columns={columns}></DataGrid>
+      </div>
+
+      {categories.map((category) => (
+        <Typography key={category.id}>{category.name}</Typography>
+      ))}
+    </Box>
   );
 }
---
+++ src/features/categories/categorySlice.ts
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+
+export interface Category {
+  id: string;
+  name: string;
+  is_active: boolean;
+  created_at: string;
+  updated_at: string;
+  description: null | string;
+  deleted_at: null | string;
+}
+
+const category: Category = {
+  id: "u1cj89ddd-d8f8-4f8f-b8f8-d8f8d8f8f8f8",
+  name: "Category 1",
+  description: "This is a description for category 1",
+  is_active: true,
+  deleted_at: null,
+  created_at: "2020-01-01T00:00:00.000Z",
+  updated_at: "2020-01-01T00:00:00.000Z",
+};
+
+const categories = [
+  category,
+  {
+    ...category,
+    id: "u1cj79ddd-d7f7-4f7f-b7f7-d7f7d7f7f7f7",
+    name: "Category 2",
+    description: "This is a description for category 2",
+  },
+  {
+    ...category,
+    id: "u1cj99ddd-d9f9-4f9f-b9f9-d9f9d9f9f9f9",
+    name: "Category 3",
+    description: "This is a description for category 3",
+  },
+];
+
+const categorySlice = createSlice({
+  name: "categories",
+  initialState: categories,
+  reducers: {
+    createCategory(state, action) {},
+    updateCategory(state, action) {},
+    deleteCategory(state, action) {},
+  },
+});
+
+export const selectCategories = (state: RootState) => state.categories;
+
+export default categorySlice.reducer;
